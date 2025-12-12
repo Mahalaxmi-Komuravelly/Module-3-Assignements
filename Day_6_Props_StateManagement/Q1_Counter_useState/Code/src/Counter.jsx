e7983ecfd5a1c40aca02109dc3487caf8f612f57
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2025-12-12 15:28:24 +0530

    Created Counter Component to Increment Count by using 'useState' React Hook

---
+++ Day_6_Props_StateManagement/Q1_Counter_useState/Code/src/Counter.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+function Counter(){
+
+    const [count,setCount] = useState(0);
+    const increment = ()=>{
+        setCount(count+1);
+    }
+
+    return (
+        <div>
+            <h3>Count : {count}</h3>
+            <button onClick={increment}>Increment Count</button>
+        </div>
+    )
+}
+
+export default Counter;
