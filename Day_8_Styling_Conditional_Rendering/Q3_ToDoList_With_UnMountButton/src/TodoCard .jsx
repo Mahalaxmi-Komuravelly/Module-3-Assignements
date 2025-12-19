a5cdcd94c7798cab1e6dd0fb3b1db03c5c197f36
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2025-12-19 11:33:11 +0530

    Iteration,Fetch and Cleanup Todo List with Unmount Button

--- Day_8_Styling_Conditional_Rendering/Q3_ToDoList_With_UnMountButton/src/TodoCard .jsx
+++ Day_8_Styling_Conditional_Rendering/Q3_ToDoList_With_UnMountButton/src/TodoCard .jsx
@@ -1,16 +1,12 @@
 function TodoCard({todos}){
     return (
-        
-            
                 todos.map(todo=>(
                 <div className="card" key={todo.id}>
                     <p>User Id: {todo.userId}</p>
                     <p>Title: {todo.title}</p>
                     <p>Completed: {todo.completed === true ? "true" : "false"}</p>
                 </div>
-            ))
-        
-        
+            ))   
     )
 }
 
